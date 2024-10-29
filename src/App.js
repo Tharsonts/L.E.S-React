84db9750afb5b3d92d60fb072be43812d2926b36
Tharsonts <134340552+Tharsonts@users.noreply.github.com>
2024-10-29 12:07:30 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,8 +3,6 @@ import {BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from "./Home/home";
 import Semana01 from "./semana01/Atv01";
 import Semana02 from "./semana02/Atv02";
-import Semana03 from "./semana03/Atividade03";
-import { Atividade04 } from "./semana04/Atividade04";
 function App() {
   return (
       <BrowserRouter>
@@ -12,8 +10,6 @@ function App() {
           <Route path="/" element={<Home/>} />
           <Route path="/semana01" element={<Semana01/>} />
           <Route path="/semana02" element={<Semana02/>} />
-          <Route path="/semana03" element={<Semana03/>} />
-          <Route path="/semana04" element={<Atividade04/>} />
       </Routes>
       </BrowserRouter>
 
