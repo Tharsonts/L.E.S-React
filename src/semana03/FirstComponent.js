457d330f830eebef7efe29ba046f8b25d76821f4
Tharsonts <134340552+Tharsonts@users.noreply.github.com>
2024-11-08 23:45:22 -0300

    Add files via upload

---
+++ src/semana03/FirstComponent.js
@@ -0,0 +1,13 @@
+export default function FirstComponent() 
+{
+    return (
+        <article>
+            <h1> My first Component </h1>
+            <ol>
+                <li> Components: UI Building Blocks </li>
+                <li> Defining a Component </li>
+                <li> Using a Component </li>
+            </ol>
+        </article>
+    );
+}
