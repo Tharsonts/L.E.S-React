47e99c1c5c05e1e3900c9957e736a32d517d0ac0
Tharsonts <134340552+Tharsonts@users.noreply.github.com>
2024-11-18 11:03:47 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from "./Home/home";
 import Semana01 from "./semana01/Atv01";
 import Semana02 from "./semana02/Atv02";
 import Semana03 from "./semana03/Atividade03";
+import Semana04 from "./semana04/Atividade04";
 function App() {
   return (
       <BrowserRouter>
@@ -12,6 +13,7 @@ function App() {
           <Route path="/semana01" element={<Semana01/>} />
           <Route path="/semana02" element={<Semana02/>} />
           <Route path="/semana03" element={<Semana03/>} />
+          <Route path="/semana04" element={<Semana04/>} />
       </Routes>
       </BrowserRouter>
 
